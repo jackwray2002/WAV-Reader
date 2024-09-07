1a61791d7e05269ed6977894f52190038b08047f
jackwray2002 <jackwray2002@gmail.com>
2024-09-07 10:58:44 -0400

    General Changes

--- WAV_Notes.py
+++ WAV_Notes.py
@@ -45,129 +45,155 @@ def generate_notes(path: path, instrument_name: str) -> None:
             if j == 0:
                 FILE.write(f"#define {instrument_name.upper()}_C{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 1:
                 FILE.write(f"#define {instrument_name.upper()}_CSHARP{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 2:
                 FILE.write(f"#define {instrument_name.upper()}_D{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 3:
                 FILE.write(f"#define {instrument_name.upper()}_DSHARP{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 4:
                 FILE.write(f"#define {instrument_name.upper()}_E{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 5:
                 FILE.write(f"#define {instrument_name.upper()}_F{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 6:
                 FILE.write(f"#define {instrument_name.upper()}_FSHARP{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 7:
                 FILE.write(f"#define {instrument_name.upper()}_G{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 8:
                 FILE.write(f"#define {instrument_name.upper()}_GSHARP{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 9:
                 FILE.write(f"#define {instrument_name.upper()}_A{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 10:
                 FILE.write(f"#define {instrument_name.upper()}_ASHARP{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
+                FILE.write('\n')
             elif j == 11:
                 FILE.write(f"#define {instrument_name.upper()}_B{i+2}_SIZE "+
                 f"{len(note_data_array[12 * i + j])}")
-    FILE.write(end='\n')
+                FILE.write('\n')
+    FILE.write('\n')
 
     # Print out data section definitions for byte arrays
     FILE.write("#if __TI_COMPILER_VERSION__")
+    FILE.write('\n')
     pragma_string = "#pragma DATA_SECTION("
     for i in range(NUM_OCTAVES):
         for j in range(NUM_NOTES):
             if j == 0:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_C{i+2}"+
                 f", \".{instrument_name.lower()}_c{i+2}\")")
+                FILE.write('\n')
             elif j == 1:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_CSHARP{i+2}"+
                 f", \".{instrument_name.lower()}_csharp{i+2}\")")
+                FILE.write('\n')
             elif j == 2:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_D{i+2}"+
                 f", \".{instrument_name.lower()}_d{i+2}\")")
+                FILE.write('\n')
             elif j == 3:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_DSHARP{i+2}"+
                 f", \".{instrument_name.lower()}_dsharp{i+2}\")")
+                FILE.write('\n')
             elif j == 4:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_E{i+2}"+
                 f", \".{instrument_name.lower()}_e{i+2}\")")
+                FILE.write('\n')
             elif j == 5:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_F{i+2}"+
                 f", \".{instrument_name.lower()}_f{i+2}\")")
+                FILE.write('\n')
             elif j == 6:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_FSHARP{i+2}"+
                 f", \".{instrument_name.lower()}_fsharp{i+2}\")")
+                FILE.write('\n')
             elif j == 7:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_G{i+2}"+
                 f", \".{instrument_name.lower()}_g{i+2}\")")
+                FILE.write('\n')
             elif j == 8:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_GSHARP{i+2}"+
                 f", \".{instrument_name.lower()}_gsharp{i+2}\")")
+                FILE.write('\n')
             elif j == 9:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_A{i+2}"+
                 f", \".{instrument_name.lower()}_a{i+2}\")")
+                FILE.write('\n')
             elif j == 10:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_ASHARP{i+2}"+
                 f", \".{instrument_name.lower()}_asharp{i+2}\")")
+                FILE.write('\n')
             elif j == 11:
                 FILE.write(f"{pragma_string}{instrument_name.upper()}_B{i+2}"+
                 f", \".{instrument_name.lower()}_b{i+2}\")")
-    FILE.write(end='\n')
+                FILE.write('\n')
+    FILE.write('\n')
 
     # Print out byte array definitions
     for i in range(NUM_OCTAVES):
         for j in range(NUM_NOTES):
             if j == 0:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_C{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 1:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_CSHARP{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 2:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_D{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 3:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_DSHARP{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 4:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_E{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 5:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_F{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 6:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_FSHARP{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 7:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_G{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 8:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_GSHARP{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 9:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_A{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 10:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_ASHARP{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
             elif j == 11:
                 FILE.write(f"const uint8_t {instrument_name.upper()}_B{i+2}"+
-                f"[{len(note_data_array[12 * i + j])}] = ", end='')
-            FILE.write("{", end='')
+                f"[{len(note_data_array[12 * i + j])}] = ")
+            FILE.write("{")
             for k, l in enumerate(note_data_array[12 * i + j]):
                 if k > 0:
-                    FILE.write(", ", end='')
-                FILE.write(str(hex(l)), end='')
+                    FILE.write(", ")
+                FILE.write(str(hex(l)))
             FILE.write("};")
+            FILE.write('\n')
     FILE.write("\n#endif\n")
     FILE.close()
 
